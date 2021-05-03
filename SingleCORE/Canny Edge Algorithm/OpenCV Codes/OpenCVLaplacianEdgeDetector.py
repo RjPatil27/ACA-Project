a6dd8dd07cf162bd470f5adaa4c5fb2e279b021d
Rajat Patil <patilrajat277@gmail.com>
2021-05-03 10:52:23 -0400

    Update OpenCVLaplacianEdgeDetector.py

--- SingleCORE/Canny Edge Algorithm/OpenCV Codes/OpenCVLaplacianEdgeDetector.py
+++ SingleCORE/Canny Edge Algorithm/OpenCV Codes/OpenCVLaplacianEdgeDetector.py
@@ -12,11 +12,11 @@ count = 0
 images = []
 
 for imagepath in path:
-    img = cv2.imread(str(imagepath),0)
+    img = cv2.imread(str(imagepath))
     imS = cv2.resize(img, (940, 600))
-    images.append(img)
-    # gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur_img = cv2.GaussianBlur(imS, (3, 3), 0)
+    images.append(imS)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur_img = cv2.GaussianBlur(gray_img, (3, 3), 0)
     laplacian = cv2.Laplacian(blur_img, cv2.CV_64F)
     plt.imshow(laplacian,cmap='gray')
     count+=1
