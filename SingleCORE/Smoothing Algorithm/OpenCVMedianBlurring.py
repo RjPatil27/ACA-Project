8ffad22bf6d0eab6c755c6bc801aae5c4c1bcb64
Rajat Patil <patilrajat277@gmail.com>
2021-05-22 17:53:26 -0400

    Update OpenCVMedianBlurring.py

--- SingleCORE/Smoothing Algorithm/OpenCVMedianBlurring.py
+++ SingleCORE/Smoothing Algorithm/OpenCVMedianBlurring.py
@@ -1,18 +1,25 @@
 import cv2
 from pathlib2 import Path
 import time
+
+start = time.time()
 path = Path(".")
 
 path = path.glob("*.jpg")
 
-# images = []
-
+images = []
+count=0
 start = time.time()
 
 for imagepath in path:
     img = cv2.imread(str(imagepath))
-#     imS = cv2.resize(img, (940, 600))
-#     images.append(img)
+    # imS = cv2.resize(img, (940, 600))
+    # images.append(imS)
     median = cv2.medianBlur(img, 3)
-    cv2.imshow('Gaussian Blurring', median)
+    # cv2.imshow('Median Blurring', median)
+    # cv2.imwrite("MedianOP_" + str(count) + ".jpg", median)
+    count+=1
     cv2.waitKey(0)
+
+end = time.time()
+print("ImageCount = ",count,"\nTimeRequired =",end-start)
