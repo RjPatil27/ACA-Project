b8377d401dd4c04a25d4b6cbb3f1b8256e331378
Rajat Patil <patilrajat277@gmail.com>
2021-05-22 17:36:39 -0400

    Update OpenCVSobelEdgeDetector.py

--- SingleCORE/Canny Edge Algorithm/OpenCV Codes/OpenCVSobelEdgeDetector.py
+++ SingleCORE/Canny Edge Algorithm/OpenCV Codes/OpenCVSobelEdgeDetector.py
@@ -11,17 +11,19 @@ count=0
 images = []
 
 for imagepath in path:
-    img = cv2.imread(str(imagepath),0)
-    imS = cv2.resize(img, (940, 600))
-    images.append(imS)
-
-    img_sobelx = cv2.Sobel(imS, cv2.CV_8U, 1, 0, ksize=5)
-    img_sobely = cv2.Sobel(imS, cv2.CV_8U, 0, 1, ksize=5)
+    img = cv2.imread(str(imagepath))
+    # imS = cv2.resize(img, (940, 600))
+    # images.append(img)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur_img = cv2.GaussianBlur(gray_img, (3, 3), 0)
+    img_sobelx = cv2.Sobel(blur_img, cv2.CV_8U, 0, 1, ksize=3)
+    img_sobely = cv2.Sobel(blur_img, cv2.CV_8U, 1, 0, ksize=3)
     img_sobel = img_sobelx + img_sobely
 
     # cv2.imshow("Sobel X", img_sobelx)
     # cv2.imshow("Sobel Y", img_sobely)
     # cv2.imshow("Sobel", img_sobel)
+    # cv2.imwrite("SobelOP_" + str(count) + ".jpg", img_sobel)
     count+=1
     cv2.waitKey(0)
 
