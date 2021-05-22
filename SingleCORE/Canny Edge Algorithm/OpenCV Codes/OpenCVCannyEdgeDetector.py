a278270781c536a40f9be5612c438fe400c59cc8
Rajat Patil <patilrajat277@gmail.com>
2021-05-22 17:18:25 -0400

    Update OpenCVCannyEdgeDetector.py

--- SingleCORE/Canny Edge Algorithm/OpenCV Codes/OpenCVCannyEdgeDetector.py
+++ SingleCORE/Canny Edge Algorithm/OpenCV Codes/OpenCVCannyEdgeDetector.py
@@ -9,20 +9,19 @@ path = Path(".")
 
 path = path.glob("*.jpg")
 count=0
-images = []
+# images = []
 
 for imagepath in path:
     img = cv2.imread(str(imagepath))
-    imS = cv2.resize(img, (940, 600))
-    images.append(imS)
-    gray = cv2.cvtColor(imS, cv2.COLOR_BGR2GRAY)
-    # img_gaussian = cv2.GaussianBlur(gray, (3, 3), 0)
-
-    img_canny = cv2.Canny(imS,100,200)
+    # imS = cv2.resize(img, (940, 600))
+    # images.append(img)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur_img = cv2.GaussianBlur(gray_img, (3, 3), 0)
+    img_canny = cv2.Canny(blur_img,50,100)
     # cv2.imshow("Canny", img_canny)
+    # cv2.imwrite("OP"+str(count)+".jpg",img_canny)
     count+=1
     cv2.waitKey(0)
 
 end = time.time()
-
 print("ImageCount = ",count,"\nTimeRequired =",end-start)
