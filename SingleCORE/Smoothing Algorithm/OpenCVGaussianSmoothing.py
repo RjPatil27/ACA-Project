ffdaf4f7029cdba37b54959cd4b0fdb795c8cfe4
Rajat Patil <patilrajat277@gmail.com>
2021-05-22 21:26:38 -0400

    Update OpenCVGaussianSmoothing.py

--- SingleCORE/Smoothing Algorithm/OpenCVGaussianSmoothing.py
+++ SingleCORE/Smoothing Algorithm/OpenCVGaussianSmoothing.py
@@ -5,20 +5,19 @@ import time
 #taking path of image files from the folder
 path = Path(".")
 path = path.glob("*.jpg")
+images = []
 count=0
-totalTime = 0
+totalTime=0
 
 for imagepath in path:
     # imread() reads single image from folder
     start = time.time()
     img = cv2.imread(str(imagepath))
-    # GaussianBlur() function convert image into Blur image which reduce noise from the image.
-    Gaussian = cv2.GaussianBlur(img,(3,3),0)
-    # cv2.imshow('Gaussian Blurring', Gaussian)
-    # cv2.imwrite("GaussianOP_" + str(count) + ".jpg", Gaussian)
-    count+=1
+    median = cv2.medianBlur(img, 3)
+    # cv2.imshow('Median Blurring', median)
+    # cv2.imwrite("MedianOP_" + str(count) + ".jpg", median)
     totalTime = totalTime + time.time() - start
+    count+=1
     cv2.waitKey(0)
 
-end = time.time()
 print("ImageCount = ",count,"\nTimeRequired =",totalTime)
