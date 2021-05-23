784118be373190d328337098f27f9048fa677bc2
Rajat Patil <patilrajat277@gmail.com>
2021-05-22 21:25:15 -0400

    Update OpenCVLaplacianEdgeDetector.py

--- SingleCORE/Canny Edge Algorithm/OpenCV Codes/OpenCVLaplacianEdgeDetector.py
+++ SingleCORE/Canny Edge Algorithm/OpenCV Codes/OpenCVLaplacianEdgeDetector.py
@@ -2,28 +2,29 @@ import cv2
 from pathlib2 import Path
 import time
 
-start = time.time()
-
+#taking path of image files from the folder
 path = Path(".")
 path = path.glob("*.jpg")
-
 count = 0
-# images = []
+totalTime=0
 
 for imagepath in path:
+    # imread() reads single image from folder
+    start = time.time()
     img = cv2.imread(str(imagepath))
-    # imS = cv2.resize(img, (940, 600))
-    # images.append(img)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # GaussianBlur() function convert image into Blur image which reduce noise from the image.
     blur_img = cv2.GaussianBlur(gray_img, (3, 3), 0)
+    # Laplacian() function applies Laplacian edge algorithm on Gaussian blurred image.
     laplacian = cv2.Laplacian(blur_img, cv2.CV_64F)
     # plt.imshow(laplacian,cmap='gray')
     # cv2.imwrite("Laplacian_" + str(count) + ".jpg", laplacian)
     count+=1
+    totalTime = totalTime + time.time() - start
     cv2.waitKey(0)
 
 end = time.time()
 
-print("ImageCount = ",count,"\nTimeRequired =",end-start)
+print("ImageCount = ",count,"\nTimeRequired =",totalTime)
 
 
